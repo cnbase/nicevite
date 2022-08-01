d4c6097ec3e30e41255c36b657914accce99f459
bzero <admin@itxiaoge.com>
2022-08-01 12:20:26 +0800

    增加配置项

---
+++ nice_app/demo/nice.config.js
@@ -0,0 +1,20 @@
+/**
+ * 项目插件
+ */
+const AppPlugins = []
+
+/**
+ * vite resolve配置
+ */
+const AppResolve = {}
+
+/**
+ * 自定义 rollupOptions 参数
+ */
+const AppRollupOptions = {}
+
+module.exports = {
+    AppPlugins,
+    AppResolve,
+    AppRollupOptions
+}
