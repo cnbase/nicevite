879f5b4bd854fe2ef81f7fb3a50a7a44ba4e1a97
bzero <admin@itxiaoge.com>
2022-08-05 16:01:03 +0800

    增加iconFont图标支持

---
+++ nice_app/nice_admin/admin_bak/index.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './Index.vue'
+// import { Button, message } from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css'
+
+const app = createApp(App)
+
+// app.use(Button)
+// app.config.globalProperties.$message = message;
+
+app.mount('#app')
